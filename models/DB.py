f56a756695b219d816795cc8771fae1cd4e4182a
Niels <nga.smits@student.avans.nl>
2020-03-31 13:00:20 +0200

    Db class

---
+++ models/DB.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+class DB(object):
+
+    def __init__(self):
+        self.open_db()
+
+    def open_db(self):
+        self.db = sqlite3.connect('../mastermind.db')
+        self.cursor = self.connection.cursor()
+        self.connected = True
+
+    def execute(self, query, bindings):
+        self.open_db()
+        cur = self.db.cursor()
+        cur.execute(query, bindings)
+
+    def query(self, query, bindings=None):
+        self.open_db()
+        curs = self.db.cursor()
+        if bindings:
+            curs.execute(query, bindings)
+        else:
+            curs.execute(query)
+
+        while True:
+            row = curs.fetchone()
+            if not row:
+                return None
+            yield row
+
+        self.close()
+
+    def close(self):
+        self.db.commit()
+        self.db.close()
+        self.connected = False
+
+
+
+
+
